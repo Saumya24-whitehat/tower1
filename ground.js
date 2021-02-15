a52a037cb6117adb4504ff7a4a74e8c2087f1e79
nila12310 <75618531+nila12310@users.noreply.github.com>
2021-02-15 09:45:48 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -10,7 +10,7 @@ class Ground{
     }
     display(){
         noStroke();
-        fill(188,67,67);
+        fill("red");
         rectMode(CENTER);
         rect(this.ground.position.x,this.ground.position.y,900,20);
     }
